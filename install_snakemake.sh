4ddc9d97607b59324f5d4e018e0ee1fa29cfc0f2
zouter <wouter.saelens@gmail.com>
2018-10-11 21:23:47 +0200

    add install snakemake for travis

---
+++ install_snakemake.sh
@@ -0,0 +1,18 @@
+# based on https://conda.io/docs/user-guide/tasks/use-conda-with-travis-ci.html
+
+# set default python version if not present
+TRAVIS_PYTHON_VERSION=${TRAVIS_PYTHON_VERSION:=3.6}
+
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+
+conda info -a
+conda config --add channels defaults
+conda config --add channels conda-forge
+conda create -q -n snakemake python=$TRAVIS_PYTHON_VERSION snakemake
+
+source activate snakemake
