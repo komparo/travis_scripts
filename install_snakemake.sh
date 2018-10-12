95330d07649b0563a28aaea9030f5c90227021ea
zouter <wouter.saelens@gmail.com>
2018-10-12 15:05:49 +0200

    update existing conda installation

--- install_snakemake.sh
+++ install_snakemake.sh
@@ -5,7 +5,7 @@ TRAVIS_PYTHON_VERSION=${TRAVIS_PYTHON_VERSION:=3.6}
 
 # download miniconda
 wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-bash miniconda.sh -b -p $HOME/miniconda
+bash miniconda.sh -b -p -u $HOME/miniconda
 
 # update conda
 export PATH="$HOME/miniconda/bin:$PATH"
