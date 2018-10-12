7adcf8ba36c75f1cf43bf2b90399ee737d0089bb
zouter <wouter.saelens@gmail.com>
2018-10-12 14:15:49 +0200

    shut up wget

--- install_snakemake.sh
+++ install_snakemake.sh
@@ -3,7 +3,7 @@
 # set default python version if not present
 TRAVIS_PYTHON_VERSION=${TRAVIS_PYTHON_VERSION:=3.6}
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 hash -r
