6774a11ea1cab3deb39d65f2e8f5d76aa871c1a1
zouter <wouter.saelens@gmail.com>
2018-10-12 14:34:11 +0200

    check if miniconda is already installed

--- install_snakemake.sh
+++ install_snakemake.sh
@@ -3,8 +3,11 @@
 # set default python version if not present
 TRAVIS_PYTHON_VERSION=${TRAVIS_PYTHON_VERSION:=3.6}
 
-wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-bash miniconda.sh -b -p $HOME/miniconda
+if [ -d "${HOME}/miniconda" ]; then
+	wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+	bash miniconda.sh -b -p $HOME/miniconda
+fi
+
 export PATH="$HOME/miniconda/bin:$PATH"
 hash -r
 conda config --set always_yes yes --set changeps1 no
